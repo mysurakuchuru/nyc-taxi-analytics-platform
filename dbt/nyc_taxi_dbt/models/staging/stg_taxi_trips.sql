fed565d7ab8c273c21caccf863912ad4f51a2d57
Mysura Reddy Kuchuru <mysurareddykuchuru@Mysuras-MacBook-Pro.local>
2026-06-11 13:25:21 -0400

    Add first dbt staging model

---
+++ dbt/nyc_taxi_dbt/models/staging/stg_taxi_trips.sql
@@ -0,0 +1,14 @@
+SELECT
+    vendor_id,
+    tpep_pickup_datetime,
+    tpep_dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    payment_type,
+    fare_amount,
+    tip_amount,
+    total_amount,
+    pu_location_id,
+    do_location_id,
+    source_file
+FROM staging.yellow_taxi_trips_clean
